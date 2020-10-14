eeb4ea51aed44cced6e475f57ca4939c0f19da2a
Tworit Dash <tworitdash@Tworits-MacBook-Pro.local>
2020-10-15 00:06:11 +0200

    Static

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"Introduction On this day, I had this idea to blog my daily activities. I started my Ph.D. at the Delft University of Technology on 1st September, 2020. I thought it is a good idea to write the activities I do everyday on a blog. This blog is built with jekyll...","categories": [],
         "tags": [],
         "url": "/oct12/"
+      },{
+        "title": "Oct14",
+        "excerpt":"The schedule Today, I started my day a little late, around 9h30 EST. I wanted to study, but couldn’t concentrate well. However, I started a little bit and took intermittent breaks. Somehow, I managed to read two lectures of a course. Then, I cooked lunch and had it together with...","categories": [],
+        "tags": [],
+        "url": "/oct13/"
       }]
