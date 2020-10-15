f9c465477c5cc5fc8951548f489c103aa4f6e4f2
Tworit Dash <tworitdash@Tworits-MacBook-Pro.local>
2020-10-16 01:07:48 +0200

    Static

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"The schedule Today, I started my day a little late, around 9h30 EST. I wanted to study, but couldn’t concentrate well. However, I started a little bit and took intermittent breaks. Somehow, I managed to read two lectures of a course. Then, I cooked lunch and had it together with...","categories": [],
         "tags": [],
         "url": "/oct13/"
+      },{
+        "title": "Oct15",
+        "excerpt":"The schedule Started the day late at almost 9h00 EST. I wanted to study and when I looked at the lecture slides of sensor signal and data processing (SSDP), I was a little lost. Therefore, I started looking for YouTube videos explaining the same thing. I had a nice sandwich...","categories": [],
+        "tags": [],
+        "url": "/oct15/"
       }]
